55c9393fcfce9bb49596a187a34ad42895814af6
Tema Nikitin <megadestroyer95@gmail.com>
2019-11-09 23:30:31 +0300

    исправил ошибку с админкой в билде

---
+++ dist/admin/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import App from './App.vue';
+import axios from 'axios';
+import { store } from './store';
+import SimpleVueValidation from 'simple-vue-validator';
+Vue.use(SimpleVueValidation);
+
+new Vue({
+    el: "#app-root",
+    render: h => h(App),
+    store
+
+
+});
